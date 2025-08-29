721286de159780d9a6d4b41d6ed1e6e4d318df8b
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-08-29 16:52:23 -0300

    cree el modelo schema de un color hexadecimal

---
+++ src/models/color.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const colorSchema = new Schema({
+  inputTarea: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+    unique: true,
+    validate: {
+      validator: (valor) => {
+        return /^#[0-9A-Fa-f]{6}$/.test(
+          valor
+        );
+      },
+    },
+  },
+});
+
+const Color = mongoose.model("color", colorSchema);
+
+export default Color;
--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -2,11 +2,6 @@ import { Router } from "express";
 
 const router = Router()
 
-router.use('/test',(req,res)=>{
-    res.status(200)
-    res.send('Primera prueba desde Backend')
-})
-
 router.route('/')
 router.route('/:id')
 
